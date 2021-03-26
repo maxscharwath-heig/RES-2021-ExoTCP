522340cf4870a247b00a134d3543ed45aa36955c
maxscharwath <maxscharwath@gmail.com>
2021-03-26 11:37:32 +0100

    Maven

--- labo/src/ch/heigvd/res/tscp/server/Server.java
+++
@@ -1,8 +0,0 @@
-package ch.heigvd.res.tscp.server;
-
-public class Server {
-    public static void main(String[] args) {
-        SingleThreadedServer single = new SingleThreadedServer(2203);
-        single.serveClients();
-    }
-}
